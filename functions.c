f358639f34d30fb6a6e4a10c803314e38ffa8e4e
Sarah Leong <sleong20@stuy.edu>
2021-10-04 17:37:56 -0400

    added avgArr

--- functions.c
+++ functions.c
@@ -10,16 +10,36 @@ void populate(int arr[], int arrSize); // helper
 int main() {
     int a[10];
     populate(a, sizeof(a)/sizeof(int));
-    printArr(a, sizeof(a)/sizeof(int));
     int b[15];
     populate(b, sizeof(b)/sizeof(int));
-    printArr(b, sizeof(b)/sizeof(int));
     int c[20];
     populate(c, sizeof(c)/sizeof(int));
-    printArr(c, sizeof(c)/sizeof(int));
     int d[10];
     populate(d, sizeof(d)/sizeof(int));
-    printArr(d, sizeof(d)/sizeof(int));
+    int e[10];
+    int i;
+    for (i = 0; i<10; i++) {
+        e[i] = i;
+    }
+
+    // testing printArr
+    // printf("array a:\n");
+    // printArr(a, sizeof(a)/sizeof(int));
+    // printf("array b:\n");
+    // printArr(b, sizeof(b)/sizeof(int));
+    // printf("array c:\n");
+    // printArr(c, sizeof(c)/sizeof(int));
+    // printf("array d:\n");
+    // printArr(d, sizeof(d)/sizeof(int));
+
+    // testing avgArr
+    printf("array e: \n");
+    printArr(e, sizeof(e)/sizeof(int));
+    printf("avg of e: %lf\n\n", avgArr(e, sizeof(e)/sizeof(int)));
+    
+    printf("array a: \n");
+    printArr(a, sizeof(a)/sizeof(int));
+    printf("avg of a: %lf\n", avgArr(a, sizeof(a)/sizeof(int)));
 
     return 0;
 }
@@ -32,6 +52,16 @@ void printArr(int arr[],int arrSize) {
         printf("%d ", arr[i]);
     }
     printf("]\n");
+}
+
+double avgArr(int arr[], int arrSize) {
+    long total = 0;
+    int i;
+    
+    for (i = 0; i < arrSize; i++) {
+        total += arr[i];
+    }
+    return (double)total/(double)arrSize;
 }
 
 void populate(int arr[], int arrSize) { // helper
